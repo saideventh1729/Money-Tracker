92faf0ef1b6f142d8f5ff9a42b0f82996cf3926c
saideventh1729 <102856897+saideventh1729@users.noreply.github.com>
2022-06-08 13:08:09 +0530

    updated controller

--- src/main/java/com/example/demo/controller/MoneyController.java
+++ src/main/java/com/example/demo/controller/MoneyController.java
@@ -27,5 +27,10 @@ public class MoneyController {
 	public ResponseEntity getAllMoney (@PathVariable Integer moneyId){
 		return ResponseEntity.ok().body(imoneyservice.getAllMoney(moneyId));
 	}
+	@GetMapping("")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 
 }
